d2ae63075d32ea3b126d6ff7e24c3e33af115985
TahaSekk <sohailtaha128@gmail.com>
2022-07-23 03:01:40 +0500

    commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
 //select 
-let h1= document.querySelector('h1');
-let button=document.querySelector('button');
+// let h1= document.querySelector('h1');
+// let button=document.querySelector('button');
+
+// button.addEventListener('click',change=()=>{
+//     h1.innerText='hello'
+// });
