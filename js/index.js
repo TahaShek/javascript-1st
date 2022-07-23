4648cd4edaed104b9e3c03ab1f6c95e44cb5f6db
TahaSekk <sohailtaha128@gmail.com>
2022-07-24 02:57:30 +0500

    coomit

--- js/index.js
+++
@@ -1,7 +0,0 @@
-//select 
-// let h1= document.querySelector('h1');
-// let button=document.querySelector('button');
-
-// button.addEventListener('click',change=()=>{
-//     h1.innerText='hello'
-// });
