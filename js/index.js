364ee372ab9cb3454ac8fcab67323676c48c6824
TahaSekk <sohailtaha128@gmail.com>
2022-07-24 06:27:39 +0500

    commit

--- js/index.js
+++ js/index.js
@@ -1,16 +1,24 @@
 const numbers=document.querySelectorAll('.btn');
 const Result=document.querySelector('input');
 const operations=document.querySelectorAll('.operation-btn');
-const AC=document.querySelector('.AC');
-const C=document.querySelector('.C');
+const AC=document.querySelector('.Ac');
+const C=document.querySelector('.Clear');
 const equalBtn=document.querySelector('.equal');
 
 
 let inputValue='';
+// let haveDot=false;
 
 numbers.forEach(btn => {
     btn.addEventListener('click',function(){
         // console.log(btn)
+        // if(Result.innerText==='.' && !haveDot){
+        //     haveDot=true;
+        // }
+        // else if ( Result.innerText ==='.'&& have){
+        //     return;
+           
+        // }
    inputValue += this.innerText;
    Result.value=inputValue; 
     })
@@ -21,7 +29,43 @@ numbers.forEach(btn => {
 operations.forEach(operations => {
   operations.addEventListener('click', function () {
     // console.log(this.textContent);
+    
     inputValue +=this.innerText ;
     Result.value = inputValue;
   });
 });
+
+
+
+
+AC.addEventListener('click', function () {
+Result.value = '0';
+    inputValue = '';
+  });
+
+
+
+ C.addEventListener('click',function(){
+    try {
+        if(Result.value.length===1){
+            Result.value='0';
+            inputValue=''
+        }
+        else{inputValue =Result.value.slice(0,-1)
+            Result.value=inputValue;  
+        }
+
+    } catch (error) {
+        alert(error.message);
+    }
+ });
+
+ equalBtn.addEventListener('click',function(){
+Result.value=eval(inputValue);
+inputValue='';
+ } );
+
+
+
+
+
