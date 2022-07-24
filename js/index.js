d111b0147fcadd013bc3f798c0e56efd5d0e514d
TahaSekk <sohailtaha128@gmail.com>
2022-07-24 05:37:43 +0500

    commit

--- js/index.js
+++ js/index.js
@@ -1,12 +1,11 @@
-const  display1=document.querySelector('.display-1');
-const  display2=document.querySelector('.display-2');
-const  TemResult=document.querySelector('.temp-result');
-const Allnumber=documnet.querySelectorAll('.numbers');
-const operations=document.querySelectorAll('.operations')
-const equal=document.querySelector('.equal');
-const clear=document.querySelector('.all-clear');
-const lastEntityClear=document.querySelector('.last-entity-clear');
-
+const display1 = document.querySelector(".display-1");
+const display2 = document.querySelector(".display-2");
+const tempResult = document.querySelector(".temp-result");
+const numbers = document.querySelectorAll(".number");
+const operation = document.querySelectorAll(".operation");
+const equal = document.querySelector(".equal");
+const clearAll = document.querySelector(".all-clear");
+const clearLast = document.querySelector(".last-entity-clear");
 
 
 let dis1Num='';
@@ -15,13 +14,37 @@ let result=null;
 let lastOperation='';
 let haveDot=false;
 
-Allnumber.array.forEach(number => {
-    number.addEventListner('click',(g)=>{
+// Agar dot aik dfa print go gaya ha tu dubara ni print hona chhaiya 
+
+
+numbers.forEach(number=>{
+    number.addEventListener('click',(g)=>{
         try {
-            
+            if(g.target.innerText==='.' && !haveDot){
+                haveDot=true
+            }
+            else if (g.target.innerText==='.'  && haveDot){
+                return;
+            }
+
+            dis2Num+=g.target.innerText;
+            display2.innerText=dis2Num;
+
         } catch (error) {
             alert(error.message)
         }
-
     })
 });
+
+
+
+operation.forEach(operation=>{
+    operation.addEventListener('click',(g)=>{
+        try {
+            // console.log('working')
+            if()
+        } catch (error) {
+            alert(error.message)
+        }
+    })
+})
